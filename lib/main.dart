37e49b8bf7fc18c4347f875533b7ad92b4d87092
jwchan1996 <741755613@qq.com>
2019-08-18 22:18:26 +0800

    refactor: 添加启动页&&调整程序结构

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './app.dart';
+import './views/Index/index.dart';
 
 void main() => runApp(Main());
 
@@ -10,19 +11,12 @@ class Main extends StatelessWidget {
     return MaterialApp(
       title: 'CliCli',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.teal,
-
       ),
       home: MyHomePage(title: 'CliCli'),
+      routes: <String, WidgetBuilder>{
+        '/Index': (BuildContext context) => new Index()
+      },
     );
   }
 }
--- lib/views/Index/index.dart
+++ lib/views/Index/index.dart
@@ -1,30 +1,10 @@
 import 'package:flutter/material.dart';
 
-class Index extends StatefulWidget {
-  Index({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _IndexState createState() => _IndexState();
-}
-
-class _IndexState extends State<Index> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class Index extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true
-      ),
+      appBar: AppBar(title: Text('CliCli'), centerTitle: true),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -33,16 +13,11 @@ class _IndexState extends State<Index> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '666',
               style: Theme.of(context).textTheme.display4,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
